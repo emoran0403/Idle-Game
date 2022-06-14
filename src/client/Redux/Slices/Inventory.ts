5a6f6dd1a9891149c98b49ca8ce222bfc4dfc13d
Eric Moran <emoran0403@gmail.com>
2022-06-14 09:57:08 -0400

    adding logic to inventory slice

--- src/client/Redux/Slices/Inventory.ts
+++ src/client/Redux/Slices/Inventory.ts
@@ -13,26 +13,42 @@ export const Inventory = createSlice({
     Coins: 0,
   },
   reducers: {
-    // use this when we need to add an item to the bank
+    /**
+     * This will need to respect the players resources setting - to bank or not to bank, that is the question
+     */
+    // use this when we need to add an item to the inventory
     addItemToInventory: (state: Types.IFlatObjectOfNums, action) => {
       const item: string = action.type; // decide which item to add
-      const amount: number = Number(action.payload); // this will be the number of items added to the bank
-
-      //Decide if the inventory has space
-
+      const amount: number = Number(action.payload); // this will be the number of items added to the inventory
       const inventorySpace: number = Object.keys(state).length;
-      if (inventorySpace < 27) {
-        //! will state[item] += amount add the key and value to the object if it does not already exist?
-        state[item] += amount; // add the item to state, then reassign (ty immer)
+      const playerIsBanking: boolean = true; //@ will need to grab this from state
+
+      // check if the player is banking or dropping items
+      if (playerIsBanking) {
+        //Decide if the inventory has space
+        if (inventorySpace < 27) {
+          //! will state[item] += amount add the key and value to the state object if it does not already exist?
+          state[item] += amount; // add the item to state, then reassign
+          //! I need to check this again, but do i need a different variable, or will it be updated upon adding the item to the state object?
+          const inventorySpaceAfterAdding: number = Object.keys(state).length;
+
+          if (inventorySpaceAfterAdding === 28) {
+            //@ this will need to trigger the player to move to the bank to deposit if the inventory is full
+            // go to bank here
+          }
+        }
+      } else {
+        //do nothing
+        //! do i need to return state here, or literally do nothing?
       }
     },
 
-    // use this when we need to remove an item from the bank
+    // use this when we need to remove an item from the inventory
     removeItemFromInventory: (state: Types.IFlatObjectOfNums, action) => {
       const item: string = action.type; // decide which item to remove
       const amount: number = Number(action.payload); // this will be the number of items removed from the bank
 
-      //@ this will need to add to the player Bank
+      //@ this will need to add to the player Bank if the player is depositing the inventory into the bank
       if (state[item] - amount >= 0) {
         // prevent the removal of an amount that would result in a negative
         state[item] -= amount; // subtract the item from state
