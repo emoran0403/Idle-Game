e4ebef5415251f2a771aa8daaae37398d7206191
Eric Moran <emoran0403@gmail.com>
2022-06-17 11:01:08 -0400

    quest list component displays a selection of quests, need to unpack the whole object for this view

--- src/client/GameComponents/LeftColumn/QuestList.tsx
+++ src/client/GameComponents/LeftColumn/QuestList.tsx
@@ -7,22 +7,24 @@ const QuestList = (props: Types.NoProps) => {
   const LumQuests = AllQuests.Lumbridge;
   useEffect(() => {}, []);
   return (
-    <div>
-      <div>This is QuestList</div>
-      {LumQuests.map((quest) => (
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{quest.name}</h5>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-header text-center">Quest List</h5>
+        {LumQuests.map((quest) => (
+          <div key={`quest-list-${quest.name}`} className="card">
+            <div className="card-body">
+              <h5 className="card-title">{quest.name}</h5>
 
-            {quest.complete && <div>Completed!</div>}
-            {!quest.complete && (
-              <div>
-                In Progress : {quest.stepsComplete} / {quest.stepsTotal}
-              </div>
-            )}
+              {quest.complete && <div>Completed!</div>}
+              {!quest.complete && (
+                <div>
+                  In Progress : {quest.stepsComplete} / {quest.stepsTotal}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
