009f70626759ce93a5a570fe1fcbd4c2b19194fd
Eric Moran <emoran0403@gmail.com>
2022-06-17 10:55:15 -0400

    imported all quests object into all quest list, building out component

--- src/client/GameComponents/LeftColumn/QuestList.tsx
+++ src/client/GameComponents/LeftColumn/QuestList.tsx
@@ -1,23 +1,30 @@
 import * as Types from "../../../../Types";
 import * as React from "react";
 import { useEffect } from "react";
+import { AllQuests } from "../../../../Constants/Quests/";
 
 const QuestList = (props: Types.NoProps) => {
+  const LumQuests = AllQuests.Lumbridge;
   useEffect(() => {}, []);
   return (
     <div>
       <div>This is QuestList</div>
+      {LumQuests.map((quest) => (
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{quest.name}</h5>
+
+            {quest.complete && <div>Completed!</div>}
+            {!quest.complete && (
+              <div>
+                In Progress : {quest.stepsComplete} / {quest.stepsTotal}
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default QuestList;
-
-{
-  /* <div className="card">
-    <div className="card-body">
-      <h5 className="card-title">Stuff</h5>
-      <div>stuff map here</div>
-    </div>
-  </div> */
-}
