a8c5184dc6ec5a828d3e00f4c7f6c153df19fd35
Eric Moran <emoran0403@gmail.com>
2022-06-25 09:42:39 -0400

    installed dayjs for timestamps on chat logs

--- src/client/GameComponents/LeftColumn/ChatWindow.tsx
+++ src/client/GameComponents/LeftColumn/ChatWindow.tsx
@@ -1,8 +1,9 @@
 import * as Types from "../../../../Types";
 import * as React from "react";
+import { useState } from "react";
 
 const ChatWindow = (props: Types.NoProps) => {
-  // gets the player's experience
+  const [chatLogArray, setChatLogArray] = useState<[]>([]);
 
   return (
     <div className="card border border-dark border-2 rounded-3" style={{ overflowY: "auto", position: "relative", height: "33%" }}>
@@ -14,3 +15,14 @@ const ChatWindow = (props: Types.NoProps) => {
 };
 
 export default ChatWindow;
+
+//! work this just like chirper
+/**
+ * map over an array of 50 objects
+ * if over 50 objects, remove the first object
+ */
+const gamelog = {
+  timeStamp: Date.now(), // or something like this
+  message: `some message here`,
+  tags: [`woodcutting`, `monster drop`, `rare item`, `level up`, `gained xp`],
+};
