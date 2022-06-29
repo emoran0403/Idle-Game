28ed4939ba570c03aa6d41a1011108fd17a8a348
Eric Moran <emoran0403@gmail.com>
2022-06-29 14:12:48 -0400

    added a hitchance function to calculate the chance of landing a hit

--- Constants/Enemies/Enemies.tsx
+++ Constants/Enemies/Enemies.tsx
@@ -6,6 +6,32 @@ const man = {
   lifePoints: 150,
   XPGivenCombatStyle: 25,
   XPGivenConstitution: 8,
+  affinities: {
+    explicitWeakness: `fireSpells`,
+    weakStyle: `magic`,
+    ownStyle: `melee`,
+    strongStyle: `ranged`,
+  },
+  armor: 130,
+  defence: 3,
+  accuracy: 130,
+};
+
+const goblin = {
+  name: `goblin`,
+  level: 2,
+  lifePoints: 100,
+  XPGivenCombatStyle: 25,
+  XPGivenConstitution: 8,
+  affinities: {
+    explicitWeakness: `airSpells`,
+    weakStyle: `magic`,
+    ownStyle: `melee`,
+    strongStyle: `ranged`,
+  },
+  armor: 120,
+  defence: 2,
+  accuracy: 120,
 };
 
 /**
@@ -20,4 +46,57 @@ const man = {
 /**
  * add a drop function to roll drops
  * define tables for drops based on ammunition, runes, fish...
+ *
+ *
+ *
+ * needs offensive stats
+ * needs a combat style used
+ *
+ * needs defensive stats
+ * will change based on player combat style choice
+ *
+ *hit chance = affinity * (accuracy / target armor rating)
+ affinity =
+  90 for explicit weakness
+  65 for weakness style
+  55 for own style
+  45 for strong style 
+ *
+
+ * accuracy from level = 0.0008 * L^3 +4L +40
+ * accuracy from weapon tier bonus = 0.0008 * T^3 +4T +40
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+const hitchance = (
+  affinity: number,
+  armorRating: number,
+  accuracy: number,
+  additiveAccuracyBoosts: number = 0,
+  multiplicativeAccuracyBoosts: number = 1
+) => {
+  // calculates the hitchance based on combat variables
+  let hitChance: number = affinity * Math.round((accuracy * multiplicativeAccuracyBoosts + additiveAccuracyBoosts) / armorRating);
+  return hitChance;
+};
+
+const executeHit = () => {
+  // calculates the hitchance, determines if a hit happens, rolls for damage
+};
+
+const damageDealt = () => {
+  // calculates the potential damageDealt based on combat variables
+};
+
+/**
+ * calculate hitchance for both parties
+ * calculate damage for both parties
+ *
+ * apply damage if random number passes the hitchance threshold
+ * if damage is enough to kill, then reward drops
  */
