e16a057e951347d4db62937ffbdf9198bd7744fc
Eric Moran <emoran0403@gmail.com>
2022-06-29 15:27:25 -0400

    added a constants file for armor

---
+++ Constants/Armor/HeadSlot.ts
@@ -0,0 +1,20 @@
+import * as Types from "../../Types";
+
+const listOfMeleeHeads = {
+  bronzeFullHelm: {
+    name: `bronzeFullHelm`,
+    levelReqDefence: 1,
+    tier: 5,
+    armor: 5,
+    lifePointsExtra: 0,
+    prayerPointsExtra: 0,
+    damageReduction: 0.02,
+    styleBonus: 0,
+    value: 168,
+  },
+};
+const listOfMagicHeads = {};
+const listOfRangedHeads = {};
+
+//! fill this out, maybe 1 or 2 for each style and slot
+//! make a tool for this lol
