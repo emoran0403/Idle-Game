4954e55ceb6266cef0fbab91391c299d536453ae
Eric Moran <emoran0403@gmail.com>
2022-07-06 09:44:07 -0400

    getArmorObject2 scrapes data as expected

--- Utilities/getArmorObject2.js
+++ Utilities/getArmorObject2.js
@@ -3,38 +3,45 @@
     name: document.title.replaceAll(` `, ``).split(`-`)[0].toLowerCase(),
 
     levelReqDefence: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[2].children[0].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[1].children[0].textContent
     ),
+
     tier: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[2].children[1].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[5].children[0].textContent
     ),
 
     armor: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[4].children[3].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[12].children[1].textContent
     ),
 
     lifePointsExtra: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[5].children[1].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[13].children[1].textContent
     ),
 
     prayerPointsExtra: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[6].children[4].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[14].children[1].textContent
     ),
 
     damageReduction: Number(
       document
         .getElementsByClassName("plainlinks infobox-bonuses wikitable")[0]
-        .children[0].children[8].children[3].textContent.slice(5, -1)
+        .children[0].children[12].children[2].children[1].textContent.substring(
+          document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[12].children[2].children[1]
+            .textContent.length - 1,
+          0
+        )
     ),
 
     styleBonusMelee: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[10].children[2].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[14].children[2].textContent
     ),
+
     styleBonusRanged: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[10].children[3].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[14].children[3].textContent
     ),
+
     styleBonusMagic: Number(
-      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[10].children[4].textContent
+      document.getElementsByClassName("plainlinks infobox-bonuses wikitable")[0].children[0].children[14].children[4].textContent
     ),
 
     value: 0,
