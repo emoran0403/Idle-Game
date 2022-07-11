d47e63ecb729079aaa20d3633c55f2e3c02bb624
Eric Moran <emoran0403@gmail.com>
2022-07-11 10:15:42 -0400

    finished getEnemyInfo tool, need to enter affinities manually due to inconsistent wiki layout

--- Utilities/getEnemyObject1.js
+++ Utilities/getEnemyObject1.js
@@ -5,22 +5,21 @@
  * I made this tool to be saved as a bookmark, click the bookmark to run the function, and open the console for the enemy info object.
  */
 
+//@ table body -    document.getElementsByTagName(`tbody`)[0].children
+
 (() => {
   let ENEMYINFO = {
-    name: `man`,
-    level: 4,
-    lifePoints: 150,
-    XPGivenCombatStyle: 25,
-    XPGivenConstitution: 8,
-    affinities: {
-      explicitWeakness: `spells-fire`,
-      weakStyle: `magic`,
-      ownStyle: `melee`,
-      strongStyle: `ranged`,
-    },
-    armor: 130,
-    defence: 3,
-    accuracy: 130,
+    name: document.getElementById(`firstHeading`).textContent.toLowerCase(),
+    level: Number(document.getElementsByTagName(`tbody`)[0].children[12].children[0].textContent),
+    lifePoints: Number(document.getElementsByTagName(`tbody`)[0].children[12].children[1].textContent),
+    XPGivenCombatStyle: Math.round(Number(document.getElementsByTagName(`tbody`)[0].children[12].children[2].textContent)),
+    XPGivenConstitution: Math.round(Number(document.getElementsByTagName(`tbody`)[0].children[12].children[3].textContent)),
+
+    affinities: { explicitWeakness: `enterManually`, weakStyle: `enterManually`, ownStyle: `enterManually`, strongStyle: `enterManually` },
+
+    armor: Number(document.getElementsByTagName(`tbody`)[0].children[31].children[0].textContent),
+    defence: Number(document.getElementsByTagName(`tbody`)[0].children[31].children[1].textContent),
+    accuracy: Number(document.getElementsByTagName(`tbody`)[0].children[28].children[0].textContent),
   };
 
   console.log(ENEMYINFO);
@@ -42,11 +41,3 @@ const exampleEnemyInfo = {
   defence: 3,
   accuracy: 130,
 };
-
-/**
- * table document.getElementsByClassName('plainlinks infobox-bonuses wikitable')[0]
- * table Body document.getElementsByClassName('plainlinks infobox-bonuses wikitable')[0].children[0]
- * list of table rows = document.getElementsByClassName('plainlinks infobox-bonuses wikitable')[0].children[0].children
- * list of table rows = document.getElementsByClassName('plainlinks infobox-bonuses wikitable')[0].children[0].children
- *
- */
