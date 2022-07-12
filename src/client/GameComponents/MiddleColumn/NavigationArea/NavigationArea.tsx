b008d6797d86c2ab1ae977cc308cb8b0d08ed767
Eric Moran <emoran0403@gmail.com>
2022-07-12 11:24:00 -0400

    when changing state, make sure to check the combine reducers file, i changed the names of slices, but did not update them in the file where i combine reducers, that has been fixed

--- src/client/GameComponents/MiddleColumn/NavigationArea/NavigationArea.tsx
+++ src/client/GameComponents/MiddleColumn/NavigationArea/NavigationArea.tsx
@@ -2,7 +2,6 @@ import * as Types from "../../../../../Types";
 import * as React from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-
 import Resources from "./Resources";
 import CurrentLocationComp from "./CurrentLocationComp";
 import CurrentSkillComp from "./CurrentSkillComp";
@@ -12,6 +11,7 @@ import CurrentActivityComp from "./CurrentActivityComp";
 const NavigationArea = (props: Types.NavigationAreaCompProps) => {
   const { CurrentLocation } = useSelector((state: Types.AllState) => state.Location as Types.ICurrentLocation);
   // useEffect(() => {}, []);
+
   return (
     <div className="d-flex border border-dark border-2 rounded-3">
       <button
