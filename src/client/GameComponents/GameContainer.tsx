674790d688aa1babb50fa8e7861fe1a019dcac65
Eric Moran <emoran0403@gmail.com>
2022-08-01 18:04:46 -0400

    swapping to 2 secs for prod

--- src/client/GameComponents/GameContainer.tsx
+++ src/client/GameComponents/GameContainer.tsx
@@ -479,7 +479,7 @@ const GameContainer = (props: Types.GameContainerProps) => {
       handleSavePoint();
 
       //! set this to 2000ms in production
-    }, 500);
+    }, 2000);
 
     // console.log({ interval });
     return () => clearInterval(interval);
